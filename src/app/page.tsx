cdcc90d7b635c74e6a9532a8d7ce146c0497891b
Vatsal Bateriwala <vatsalbateriwala562005@gmail.com>
2025-02-11 16:47:41 +0530

    my work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import WhatsAppChat from '@/components/WAIcon'
 import React from 'react'
+import HeroSection from '../components/front'
 
 const page = () => {
   return (
     <div>
-      
+      <HeroSection />
       <WhatsAppChat />
     </div>
   )
