cb1853b80cfdc1fefc13b14d3567000c7e7d3f19
mjgandhi2305 <138571862+mjgandhi2305@users.noreply.github.com>
2025-02-12 17:12:29 +0530

    herosection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 
 import FeaturesSection from '@/components/Features'
+import HeroSection from '@/components/ui/front'
+import CuriousSection from '@/components/ui/front2'
+import CupcakeShowcase from '@/components/ui/hero-section'
 import WhatsAppChat from '@/components/WAIcon'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
+      <HeroSection />
+      <CuriousSection />
+      <CupcakeShowcase />
       <WhatsAppChat />
       <FeaturesSection />
     </div>
