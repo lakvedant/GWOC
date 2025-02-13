58ae900bc51cdc15a226543f26082b07aa88f909
Lakshit Vedant <lakshitvedant1@gmail.com>
2025-02-13 21:28:47 +0530

    reviews and dilog finalised

--- src/components/CakeProdcutGrid.tsx
+++ src/components/CakeProdcutGrid.tsx
@@ -73,7 +73,7 @@ const ProductGrid: React.FC<ProductGridProps> = ({ selectedCategory }) => {
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 py-6 px-4 md:px-10">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-6 py-6 px-4 md:px-10">
       {products.map((product) => (
         <CakeProductCard
           key={product._id}
