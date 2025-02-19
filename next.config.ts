0409c40471f9fe0874383f8ec08fc9b0c6989289
Lakshit Vedant <lakshitvedant1@gmail.com>
2025-02-19 17:27:19 +0530

    order page done

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
-=======
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-  /* config options here */
->>>>>>> f51fbb8c576f4bd458b3dc21446c7eed1038f265
   images: {
     domains: [
       "source.unsplash.com",
